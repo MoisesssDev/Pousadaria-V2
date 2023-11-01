df1db439c658049474fb6a1a3a1456f42c3cb0e7
Moises Almeida <moisesssprg@gmail.com>
2023-11-01 17:18:29 -0300

    feat: created form guesthouse

---
+++ spec/system/user_registered_guesthouse_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+describe "user registered guesthouse" do
+  it "and see the registration form" do
+    # Arrange
+
+    # Act
+    visit root_path
+    click_on "Cadastrar"
+    fill_in "E-mail",	with: "moisesAlmeida@hotmail.com" 
+    fill_in "Senha",	with: "32415mo"
+    select "Dono de Pousada", from: "Tipo"
+    click_on "Salvar"
+
+    # Assert
+    expect(page).to have_field('Nome')
+    expect(page).to have_field('Razão social')
+    expect(page).to have_field('Telefone')
+    expect(page).to have_field('E-mail')
+    expect(page).to have_field('Endereço')
+    expect(page).to have_field('Bairro')
+    expect(page).to have_field('Estado')
+    expect(page).to have_field('Cidade')
+    expect(page).to have_field('CEP')
+    expect(page).to have_field('Meios de pagamento')
+    expect(page).to have_field('Aceita pets')
+    expect(page).to have_field('Políticas')
+    expect(page).to have_field('Check-in')
+    expect(page).to have_field('Check-out')
+  end
+  
+  it "and Guesthouse successfully registered" do
+    # Arrange
+
+    # Act
+    visit root_path
+    click_on "Cadastrar"
+    fill_in "E-mail",	with: "moisesAlmeida@hotmail.com" 
+    fill_in "Senha",	with: "32415mo"
+    select "Dono de Pousada", from: "Tipo"
+    click_on "Salvar"
+    fill_in 'Nome', with: 'Seu Zé'
+    fill_in 'Razão social', with: 'Pousada Seu Zé'
+    fill_in 'Telefone', with: '79 9 8876-9032'
+    fill_in 'E-mail', with: 'seuemail@example.com'
+    fill_in 'Endereço', with: 'Rua Alemedo, 89'
+    fill_in 'Bairro', with: 'João Pessoa'
+    fill_in 'Estado', with: 'Rio de Janeiro'
+    fill_in 'Cidade', with: 'Del Vale'
+    fill_in 'CEP', with: '54198-900'
+    fill_in 'Meios de pagamento', with: 'Cartão, Pix e Boleto'
+    check 'Aceita pets'
+    fill_in 'Políticas', with: 'Suas Políticas'
+    fill_in 'Check-in', with: '08:00 AM'
+    fill_in 'Check-out', with: '12:00 PM'
+    click_on "Salvar"
+
+    # Assert
+    expect(page).to have_field('Pousada cadastrada com sucesso')
+  end
+
+end
