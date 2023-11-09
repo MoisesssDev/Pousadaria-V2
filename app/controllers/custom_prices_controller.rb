0d03e2e871e0dc778685547ddb152bef4a9d1c53
Moises Almeida <moisesssprg@gmail.com>
2023-11-09 00:10:39 -0300

    feat: user cannot create custom prices that overlap

--- app/controllers/custom_prices_controller.rb
+++ app/controllers/custom_prices_controller.rb
@@ -6,16 +6,34 @@ class CustomPricesController < ApplicationController
   end
 
   def create
+    @guesthouse = current_user.guesthouse
     @custom_price = CustomPrice.new(custom_price_params)
+    
+    if custom_price_overlaps?(@custom_price)
 
-    if @custom_price.save
-      redirect_to room_path(@custom_price.room_id), notice: "Preço personalizado adicionado com sucesso."
+      redirect_to room_path(@custom_price.room_id), notice: "Já existe um preço definido para essa data"
+
     else
-      render :new
+      if @custom_price.save
+        redirect_to room_path(@custom_price.room_id), notice: "Preço personalizado adicionado com sucesso."
+      else
+        render "new", status: :unprocessable_entity
+      end
     end
   end
 
   private
+
+  def custom_price_overlaps?(custom_price)
+    room = Room.find(custom_price.room_id)
+    existing_prices = room.custom_prices
+    new_price_range = custom_price.start_date..custom_price.end_date
+
+    existing_prices.any? do |price|
+      price_range = price.start_date..price.end_date
+      price_range.overlaps?(new_price_range)
+    end
+  end
 
   def custom_price_params
     params.require(:custom_price).permit(:start_date, :end_date, :price, :room_id)
--- spec/system/user_register_custom_price_spec.rb
+++ spec/system/user_register_custom_price_spec.rb
@@ -103,5 +103,63 @@ describe 'user register custom price in the room' do
     expect(page).to have_content 'Preço: R$ 150,00'
 
   end
+
+  it 'and register overlapping dates' do
+    # Arrange
+    user = User.create!(email: "moisesalmeida@gmail.com", password: "110302", role: User::TYPE_OWNER)
+    guesthouse = user.create_guesthouse!(name: "Pousada Renascer", legal_name: "Razão Social da Pousada",
+                                   cnpj: "12345678901234", phone: "79 98837-7894",
+                                   email: "seu@email.com", address: "Rua Alemedo, 54", district: "Cocora",
+                                   state: "Rio de Janeiro", city: "João Pessoa", cep: "89700-218",
+                                   description: "Descrição da Pousada", accepted_payment_methods: "Cartão, Pix ou Boleto",
+                                   accepts_pets: true, policies: "Políticas de Uso da Pousada", check_in_time: "10:00:00", check_out_time: "14:00:00")
+    room = guesthouse.rooms.create!(
+      name: "Quarto Sol",
+      description: "Descrição da sala",
+      dimension: 30,
+      max_occupancy: 2,
+      daily_rate: 150.00,
+      has_private_bathroom: true,
+      has_balcony: true,
+      has_air_conditioning: true,
+      has_tv: false,
+      has_wardrobe: true,
+      is_accessible: true,
+      has_cofre: false,
+      available: true
+    )
+
+    custom_price = CustomPrice.new(start_date: "2023/12/01", end_date: "2024/01/29", price: 125.50, room: room)
+    custom_price.save
+
+    # Act
+    visit root_path
+    click_on "Entrar / Cadastrar"
+
+    fill_in "E-mail",	with: "moisesalmeida@gmail.com"
+    fill_in "Senha",	with: "110302"
+    within "form" do
+      click_on "Entrar"
+    end
+
+    click_on "Minha pousada"
+    click_on "Quarto Sol"
+    click_on "Personalizar preços"
+
+    select "Quarto Sol", from: "Selecione um quarto"
+    fill_in "Data inicial", with: "11/12/2023"
+    fill_in "Data final", with: "11/02/2024"
+    fill_in "Preço", with: 150
+
+    click_on "Salvar"
+
+
+    # Assert
+    expect(page).to have_content 'Já existe um preço definido para essa data'  
+    expect(page).to have_content 'Horários personalizados'
+    expect(page).to have_content 'Período: 01/12/2023 - 29/01/2024'
+    expect(page).to have_content 'Preço: R$ 125,50'
+
+  end
   
 end
