a87a270e6cf94002f129f605414e63c09c9b5abf
Moises Almeida <moisesssprg@gmail.com>
2023-12-06 22:52:19 -0300

    feat: (API pousadaria) get all guesthouse

--- app/controllers/api/v1/guesthouses_controller.rb
+++ app/controllers/api/v1/guesthouses_controller.rb
@@ -11,4 +11,15 @@ class Api::V1::GuesthousesController < ApplicationController
     
   end
 
+  def index
+    guesthouses = Guesthouse.all.order(:name)
+
+    if guesthouses.empty?
+      render status: :not_found, json: { message: "Nenhuma pousada encontrada" }
+    else
+      render status: :ok, json: guesthouses.as_json(except: [:created_at, :updated_at])
+    end
+
+  end
+
 end
--- spec/requests/apis/guesthouse_api_spec.rb
+++ spec/requests/apis/guesthouse_api_spec.rb
@@ -40,4 +40,61 @@ describe "Pousadaria API", type: :request do
 
   end
 
+  context "GET /api/v1/guesthouses" do
+      
+      it "list all guesthouses and order by name" do
+        #Arrange
+        user = Owner.create!(email: "moisesalmeida@gmail.com", password: "110302")
+        user.create_guesthouse!(name: "Pousada Renascer", legal_name: "Razão Social da Pousada",
+                                     cnpj: "12345678901234", phone: "79 98837-7894",
+                                     email: "seu01@email.com", address: "Rua Alemedo, 54", district: "Cocora",
+                                     state: "Rio de Janeiro", city: "Rio de Janeiro", cep: "CEP da Pousada",
+                                     description: "Descrição da Pousada", accepted_payment_methods: "Métodos de pagamento aceitos",
+                                     accepts_pets: true, policies: "Políticas de Uso da Pousada", 
+                                     check_in_time: "10:00:00", check_out_time: "14:00:00")
+
+        user2 = Owner.create!(email: "fernando@gmail.com", password: "110302")
+        user2.create_guesthouse!(name: "Pousada Azull", legal_name: "Razão Social da Pousada",
+                                  cnpj: "1298078901234", phone: "79 98837-7894",
+                                  email: "seu02@email.com", address: "Rua Alemedo, 54", district: "Cocora",
+                                  state: "Rio de Janeiro", city: "Rio de Janeiro", cep: "CEP da Pousada",
+                                  description: "Descrição da Pousada", accepted_payment_methods: "Métodos de pagamento aceitos",
+                                  accepts_pets: true, policies: "Políticas de Uso da Pousada", 
+                                  check_in_time: "10:00:00", check_out_time: "14:00:00")
+        #Act
+        get "/api/v1/guesthouses"
+
+        #Assert
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to include("application/json")
+
+        json_response = JSON.parse(response.body)
+
+        expect(json_response[0]["name"]).to include("Pousada Azull")
+        expect(json_response[1]["name"]).to include("Pousada Renascer")
+
+        expect(json_response[0].keys).not_to include("created_at")
+        expect(json_response[0].keys).not_to include("updated_at")
+
+        expect(json_response[1].keys).not_to include("created_at")
+        expect(json_response[1].keys).not_to include("updated_at")
+
+        expect(json_response.length).to eq(2) 
+      end
+
+      it "no guesthouses" do
+        #Arrange
+        
+        #Act
+        get "/api/v1/guesthouses"
+
+        #Assert
+        expect(response).to have_http_status(:not_found)
+        expect(response.content_type).to include("application/json")
+
+        json_response = JSON.parse(response.body)
+        expect(json_response["message"]).to include("Nenhuma pousada encontrada")
+      end
+  end
+
 end
